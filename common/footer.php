892175ba7c2e2e18bf7d666c88e9cd7418dd62a6
alisa-qatipi <qatipi14@hotmail.com>
2023-04-22 20:52:35 +0200

    ndryshimet e alises

---
+++ common/footer.php
@@ -0,0 +1,24 @@
+<div class="footer">
+    <div class="mainRow">
+        <div class="leftSide">
+            <div class="name">
+                <div class="logo"></div>
+                <h5 class="title">SkyKosovo</h5>
+            </div>
+            <p>
+                <small class="topText">
+                    Discover the future of flying! Fly high!
+                </small>
+            </p>
+            <div class="socialMedia">
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-facebook-f"></i>
+                <i class="fa-brands fa-twitter"></i>
+            </div>
+        </div>
+        <div class="rightSide"></div>
+    </div>
+    <div class="bottomRow">
+        <small>All rights reserved • SkyKosovo 2023</small>
+    </div>
+</div>
