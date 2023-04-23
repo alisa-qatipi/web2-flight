595995075d5acdb41bc36253a71ee6395ec0f2e1
alisa-qatipi <qatipi14@hotmail.com>
2023-04-23 15:00:19 +0200

    offer page modified

--- offers.php
+++ offers.php
@@ -14,46 +14,86 @@
 </head>
 <body>
 <?php include("common/navbar.php"); ?>
+    <div class="banner">
+        <div class="backdrop"></div>
     <h1>SkyKosovo's Offers</h1>
 
-    <div class="offerDescription">At SkyKosovo, we take pride in offering our customers exceptional value through our special offers. We understand that air travel can be expensive, and that's why we strive to provide our customers with the best deals and discounts on their flight bookings. Our special offers are designed to cater to the needs of all travelers, be it budget-conscious travelers or those looking for luxury travel experiences. From seasonal promotions to last-minute deals, we have something for everyone. Our team of experts works tirelessly to bring you the best offers on the market, ensuring that you get the best value for your money. Book with SkyKosovo today and take advantage of our special offers to make your travel experience even more memorable.</div>
+<div class="offerDescription">At SkyKosovo, we take pride in offering our customers exceptional value through our special offers. We understand that air travel can be expensive, and that's why we strive to provide our customers with the best deals and discounts on their flight bookings. Our special offers are designed to cater to the needs of all travelers, be it budget-conscious travelers or those looking for luxury travel experiences. From seasonal promotions to last-minute deals, we have something for everyone. Our team of experts works tirelessly to bring you the best offers on the market, ensuring that you get the best value for your money. Book with SkyKosovo today and take advantage of our special offers to make your travel experience even more memorable.</div>
 
+    </div>
     <div class="container">
+    <div class="cardContainer">
     <div class="card">
   <img src="./images/venice.jpg" alt="venice">
-  <h3>Using Science to brew the perfect cup of coffee</h3>
+  <div class="cardTitle">
+  <h3 class="cityName">Venice</h3>
+  <h5 class="price">$56</h5>
+  </div>
   <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
+<a class="button" href="index.php">Book now!</a>
+</div>
+    </div>
+
+    <div class="cardContainer">
+    <div class="card">
+  <img src="./images/venice.jpg" alt="venice">
+ <div class="cardTitle">
+  <h3 class="cityName">Venice</h3>
+  <h5 class="price">$56</h5>
+  </div>
+  <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
+  <a class="button" href="index.php">Book now!</a>
 </div>
 
-<div class="card">
+    </div>
+    <div class="cardContainer">
+    <div class="card">
   <img src="./images/venice.jpg" alt="venice">
-  <h3>Using Science to brew the perfect cup of coffee</h3>
+ <div class="cardTitle">
+  <h3 class="cityName">Venice</h3>
+  <h5 class="price">$56</h5>
+  </div>
   <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
+<a class="button" href="index.php">Book now!</a>
 </div>
+    </div>
 
-<div class="card">
+    <div class="cardContainer">
+    <div class="card">
   <img src="./images/venice.jpg" alt="venice">
-  <h3>Using Science to brew the perfect cup of coffee</h3>
+ <div class="cardTitle">
+  <h3 class="cityName">Venice</h3>
+  <h5 class="price">$56</h5>
+  </div>
   <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
+<a class="button" href="index.php">Book now!</a>
 </div>
+    </div>
 
-<div class="card">
+    <div class="cardContainer">
+    <div class="card">
   <img src="./images/venice.jpg" alt="venice">
-  <h3>Using Science to brew the perfect cup of coffee</h3>
+ <div class="cardTitle">
+  <h3 class="cityName">Venice</h3>
+  <h5 class="price">$56</h5>
+  </div>
   <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
+<a class="button" href="index.php">Book now!</a>
 </div>
+    </div>
 
-<div class="card">
+    <div class="cardContainer">
+    <div class="card">
   <img src="./images/venice.jpg" alt="venice">
-  <h3>Using Science to brew the perfect cup of coffee</h3>
+ <div class="cardTitle">
+  <h3 class="cityName">Venice</h3>
+  <h5 class="price">$56</h5>
+  </div>
   <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
+<a class="button" href="index.php">Book now!</a>
 </div>
+    </div>
 
-<div class="card">
-  <img src="./images/venice.jpg" alt="venice">
-  <h3>Using Science to brew the perfect cup of coffee</h3>
-  <p>Science can do everything else, so why not use it to brew better coffe? Fugiat perspiciatis nobis omnis nisi necessitatibus sed accusamus, rem beatae cum quia odio velit architecto expedita, assumenda.</p>
-</div>
     </div>
     <?php include("common/footer.php"); ?>
 </body>
